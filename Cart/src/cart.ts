37a2e884678b01460afc239f678cb8b3fe2cb9a3
adam27141 <adam.zdrodowski2714@gmail.com>
2024-06-12 13:09:19 +0200

    Początkowa implementacja klasy CarItem

--- Cart/src/cart.ts
+++ Cart/src/cart.ts
@@ -1,6 +1,19 @@
 declare var uuidv4: () => string;
 
 class CartItem {
+  name: string;
+  category: string;
+  price: number;
+  discount: number;
+  uuid: string;
+
+  constructor(name: string, category: string, price: number, discount: number) {
+    this.name = name;
+    this.category = category;
+    this.price = price;
+    this.discount = discount;
+    this.uuid = uuidv4();
+  }
   // Ma miec: Nazwę, Kategorię, Cenę, Rabat % na przedmiot, uuid
   // Ma umożliwiać:
   // - określać jego rabat procentowy
